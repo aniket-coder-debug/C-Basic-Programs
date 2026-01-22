b528723ffda16b70b00a1f26e6822aedb9abec18
Aniket Parbhane <parbhaneaniket53@gmail.com>
2026-01-22 23:49:46 +0530

    Add files via upload

---
+++ 06_promotion_demotion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int iAns;
+
+    iAns = 2*3.25+(5/2);         
+
+    printf("%d",iAns);           // 8
+
+    return 0;
+}
+
+// solving :-
+
+// iAns = ((2*3.25)+(5/2));   //promotion of 2 to 2.00
+// iAns = 6.50 + 2            //promotion of 2 to 2.00
+// iAns = 8.50                //demotion of 8.50 to 8
+// iAns = 8
