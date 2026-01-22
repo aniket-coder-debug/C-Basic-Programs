08691a4e60b2c0a8282de4693910b28691665bf8
Aniket Parbhane <parbhaneaniket53@gmail.com>
2026-01-22 23:48:57 +0530

    Fix formatting and include statements in calculation.c

--- calculation.c
+++ calculation.c
@@ -13,22 +13,22 @@ int main(void)
     int ans;
 
     printf("For addition,\nEnter two numbers:\t");
-    scanf("%d%d",&no1,&no2);                                   // 10 5
+    scanf("%d%d",&no1,&no2);                                   
     ans = addition(no1,no2);
-    printf("Addition is %d\n\n",ans);                          // 15
+    printf("Addition is %d\n\n",ans);                          
 
     subtraction();
 
-    printf("multiplication is %d\n\n",multiplication());       // 50
+    printf("multiplication is %d\n\n",multiplication());       
 
     printf("For division,\nEnter numerator:\t");                
-    scanf("%d",&no1);                                          // 10
+    scanf("%d",&no1);                                          
     printf("Enter denominator:\t");                             
-    scanf("%d",&no2);                                          // 5
+    scanf("%d",&no2);                                          
     division(no1,no2);
 
     ans = modulus();
-    printf("Remainder is %d\n\n",ans);                         // 1
+    printf("Remainder is %d\n\n",ans);                         
 
     return 0;
 }
@@ -39,7 +39,7 @@ int multiplication(void)
     int ans;
 
     printf("For multiplication,\nEnter two numbers:\t");
-    scanf("%d%d",&no1,&no2);                                   // 10 5
+    scanf("%d%d",&no1,&no2);                                   
 
     return no1*no2;
 }
@@ -53,9 +53,9 @@ int modulus(void)
     int denominator;
     int remainder;
 
-    printf("For modulus,\nEnter numerator:\t");                 // 7
+    printf("For modulus,\nEnter numerator:\t");                
     scanf("%d",&numerator);
-    printf("Enter denominator:\t");                             // 3
+    printf("Enter denominator:\t");                            
     scanf("%d",&denominator);
 
     remainder = numerator % denominator;
@@ -69,14 +69,15 @@ void subtraction(void)
     int ans;
 
     printf("For subtraction,\nEnter two numbers:\t");
-    scanf("%d%d",&no1,&no2);                                     // 10 5
+    scanf("%d%d",&no1,&no2);                                     
 
     ans = no1-no2;
 
-    printf("subtraction is %d\n\n",ans);                         // 5
+    printf("subtraction is %d\n\n",ans);                         
 }
 void division(int no1,int no2)
 {
-    printf("Quotient is %d\n\n",no1/no2);                        // 2
+    printf("Quotient is %d\n\n",no1/no2);                        
 }
+
 
